06dea169ba91c3d3141e4e52937deb4e033cafa6
Jordan Lumley <jordan.lumley@a1pos.com>
2020-06-30 14:50:10 -0400

    pull THIS JOE

--- lib/config/ConfigSettings.dart
+++ lib/config/ConfigSettings.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 
 class ConfigSettings {
-  static String API_URL = !kReleaseMode
+  static String API_URL = kReleaseMode
       ? "https://butlerbizsys.com/api/v1/"
       : "http://192.168.1.30:3002/api/v1/";
   static String WS_URL =
-      !kReleaseMode ? "wss://butlerbizsys.com" : "ws://192.168.1.30:3002";
+      kReleaseMode ? "wss://butlerbizsys.com" : "ws://192.168.1.30:3002";
 
   static String PUSHER_KEY = '78b022eea08a75c792e5';
 
