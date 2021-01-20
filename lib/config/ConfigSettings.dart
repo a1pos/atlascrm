c9a7965c7b1a6fbd08764ad8e1e00523fc7b2f69
Joe Pounds <joe.pounds@a1pos.com>
2021-01-20 11:31:36 -0500

    fix config

--- lib/config/ConfigSettings.dart
+++ lib/config/ConfigSettings.dart
@@ -2,13 +2,13 @@ import 'package:flutter/foundation.dart';
 
 class ConfigSettings {
   static String HOOK_API_URL =
-      !kReleaseMode ? "http://atlascrm.us:4433" : "http://atlascrm.us:4434";
+      kReleaseMode ? "http://atlascrm.us:4433" : "http://atlascrm.us:4434";
 
-  static String HASURA_URL = !kReleaseMode
+  static String HASURA_URL = kReleaseMode
       ? "https://hasura-prod.atlascrm.us/v1/graphql"
       : "https://hasura-dev.atlascrm.us/v1/graphql";
 
-  static String HASURA_WEBSOCKET = !kReleaseMode
+  static String HASURA_WEBSOCKET = kReleaseMode
       ? "wss://hasura-prod.atlascrm.us/v1/graphql"
       : "wss://hasura-dev.atlascrm.us/v1/graphql";
 
