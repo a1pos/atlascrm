fa1f1497d9da70b11c99681654d0c81f91cb2b6d
Joe Pounds <joe.pounds@a1pos.com>
2021-01-26 10:21:02 -0500

    config

--- lib/config/ConfigSettings.dart
+++ lib/config/ConfigSettings.dart
@@ -2,15 +2,15 @@ import 'package:flutter/foundation.dart';
 
 class ConfigSettings {
   static String HOOK_API_URL =
-      kReleaseMode ? "http://atlascrm.us:4433" : "http://atlascrm.us:4434";
+      kReleaseMode ? "http://atlascrm.us:4433" : "https://api-dev.atlascrm.us";
 
   static String HASURA_URL = kReleaseMode
       ? "http://24.154.179.10:10051/v1/graphql"
-      : "http://24.154.179.10:10050/v1/graphql";
+      : "https://hasura-dev.atlascrm.us/v1/graphql";
 
   static String HASURA_WEBSOCKET = kReleaseMode
       ? "ws://24.154.179.10:10051/v1/graphql"
-      : "ws://24.154.179.10:10050/v1/graphql";
+      : "wss://hasura-dev.atlascrm.us/v1/graphql";
 
   static String GOOGLE_TOKEN = "";
 
